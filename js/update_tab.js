1483e8e2e6d5c4381a42ff99db18a1277ed59086
Mallika Priya Khullar <mallika@icecreamlabs.com>
2017-11-18 16:41:46 +0530

    Creating the repo for the EPIC chrome extension - redirect

---
+++ js/update_tab.js
@@ -0,0 +1,17 @@
+/*global chrome,document,window */
+(function init(angular) {
+    "use strict";
+    try {
+        var url = "http://epicfoundation.org/new-tab"
+        chrome.tabs.getCurrent(function(tab) {
+            chrome.tabs.update(tab.id, {
+                "url": url,
+                "highlighted": true
+            });
+        });
+    } catch (e) {
+        // If anything goes wrong with the redirection logic, fail to custom apps page.
+        console.error(e);
+        angular.resumeBootstrap();
+    }
+})(angular);
